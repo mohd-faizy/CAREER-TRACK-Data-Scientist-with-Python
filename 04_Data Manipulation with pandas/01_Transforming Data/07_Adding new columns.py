9073b17c0361a5319501beea50e49f5bb5d63230
MOHD FAIZY <34925947+mohd-faizy@users.noreply.github.com>
2020-12-20 23:50:21 +0530

    Update 07_Adding new columns.py

--- 04_Data Manipulation with pandas/01_Transforming Data/07_Adding new columns.py
+++ 04_Data Manipulation with pandas/01_Transforming Data/07_Adding new columns.py
@@ -18,15 +18,13 @@ Instructions:
    proportion of homeless people in each state who are individuals.
 '''
 # Add total col as sum of individuals and family_members
-homelessness['total'] = homelessness['individuals'] + \
-    homelessness['family_members']
+homelessness['total'] = homelessness['individuals'] + homelessness['family_members']
 
 # Add p_individuals col as proportion of individuals
 # To get the proportion of homeless who are individuals,
 # divide the individuals column by the total column.
 
-homelessness['p_individuals'] = homelessness['individuals'] / \
-    homelessness['total']
+homelessness['p_individuals'] = homelessness['individuals'] / homelessness['total']
 
 # See the result
 print(homelessness)

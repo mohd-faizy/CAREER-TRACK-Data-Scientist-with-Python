3c56f64a840abb9d99fded52b73c2e770b9e88f1
faizy <ifaizymohd@gmail.com>
2020-12-22 01:26:51 +0530

    update

--- 04_Data Manipulation with pandas/02_Aggregating Data/08_Calculations with .groupby().py
+++ 04_Data Manipulation with pandas/02_Aggregating Data/08_Calculations with .groupby().py
@@ -43,5 +43,5 @@ sales_propn_by_type = sales_by_type / sum(sales["weekly_sales"])
 print(sales_propn_by_type)
 
 # Group by type and is_holiday; calc total weekly sales
-sales_by_type_is_holiday = ____
+sales_by_type_is_holiday = sales.groupby(['type', 'is_holiday'])[weekly_sales'].sum()
 print(sales_by_type_is_holiday)

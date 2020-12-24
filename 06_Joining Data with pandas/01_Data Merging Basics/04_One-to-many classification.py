b74bf516e4b47141b1e5987c38b042974e5f6f9b
faizy <ifaizymohd@gmail.com>
2020-12-25 04:25:45 +0530

    update

---
+++ 06_Joining Data with pandas/01_Data Merging Basics/04_One-to-many classification.py
@@ -0,0 +1,23 @@
+'''
+04 - One-to-many classification
+
+Understanding the difference between a one-to-one and one-to-many relationship 
+is a useful skill. In this exercise, consider a set of tables from an e-commerce 
+website. The hypothetical tables are the following:
+
+- A customer table with information about each customer
+- A cust_tax_info table with customers unique tax IDs
+- An orders table with information about each order
+- A products table with details about each unique product sold
+- An inventory table with information on how much total inventory is available to 
+  sell for each product
+
+Instructions:
+
+Select the relationship type that is most appropriate for the relationship between 
+the different tables: One-to-one, or One-to-many.
+'''
+# the releationship Between Product and inventory: - one to one
+# the releationship Between customer and cust_text_info: - one to one
+# the releationship Between customer and orders: - one to many
+# the releationship Between product and orders: - one to many

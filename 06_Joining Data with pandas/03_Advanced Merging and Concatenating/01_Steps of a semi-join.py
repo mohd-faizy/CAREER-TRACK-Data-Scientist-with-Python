09d61ea2722499c7dbef7fef64dcdead5d604c7f
faizy <ifaizymohd@gmail.com>
2020-12-26 18:42:03 +0530

    update

---
+++ 06_Joining Data with pandas/03_Advanced Merging and Concatenating/01_Steps of a semi-join.py
@@ -0,0 +1,17 @@
+'''
+Steps of a semi-join
+In the last video, you were shown how  to perform  a semi-join  with  pandas.
+In this exercise, you'll solidify your understanding of the necessary  steps.
+Recall that a semi-join filters the left table to only the rows where a match 
+exists in both the left and right tables.
+
+Instructions:
+
+Sort the steps in the correct order of the technique shown to perform a semi-join 
+in pandas.
+
+-> Merge the left a. right tab. on key column using an inner-join. 
+-> Search if key column in the left table is in the merged tables using .isin() 
+   Method creating Boolean Series 
+-> Subset the rows of the left table. 
+'''

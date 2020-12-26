ce9088913a059063ada5e69c69076a0e10f3ecce
faizy <ifaizymohd@gmail.com>
2020-12-27 03:59:35 +0530

    update

---
+++ 06_Joining Data with pandas/04_Merging Ordered and Time-Series Data/08_Explore financials with .query().py
@@ -0,0 +1,20 @@
+'''
+08 - Explore financials with .query()
+
+You have been given a table of financial data from some popular social network companies 
+called social_fin. All of the values are in thousands of US dollars.
+
+Use the .query() method and the console to explore social_fin and select the True statement.
+
+Instructions
+
+Possible Answers
+
+1. There 2 rows where the value is greater than $50,000,000K.[X]
+
+2. There are 3 rows for total revenue for Facebook.[X]
+
+3. There are 6 rows where the net income has a negative value.[✓]
+
+4. There are 45 rows, where the gross profit is greater than $100K.[X]
+'''

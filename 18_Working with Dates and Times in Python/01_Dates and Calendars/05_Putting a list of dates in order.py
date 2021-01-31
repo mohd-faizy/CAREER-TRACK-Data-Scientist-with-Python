9fc2961282b30482cdf7265e0295a20265635879
faizy <ifaizymohd@gmail.com>
2021-02-01 01:15:27 +0530

    update

---
+++ 18_Working with Dates and Times in Python/01_Dates and Calendars/05_Putting a list of dates in order.py
@@ -0,0 +1,29 @@
+'''
+05 - Putting a list of dates in order
+
+Much like numbers and strings, date objects in Python can  be  put 
+in order. Earlier dates come before later ones, and so we can sort 
+a list of date objects from earliest to latest.
+
+What  if  our Florida  hurricane  dates had  been  scrambled? We've  gone 
+ahead and  shuffled  them so  they're  in  random  order  and  saved  the 
+results as dates_scrambled. Your job is to put them back in chronological 
+order, and then print the first and last dates from this sorted list.
+
+Instructions:
+
+- Print the first and last dates in dates_scrambled.
+- Sort dates_scrambled using Python's built-in sorted() method, and save the 
+  results to dates_ordered.
+- Print the first and last dates in dates_ordered.
+'''
+# Print the first and last scrambled dates
+print(dates_scrambled[0])
+print(dates_scrambled[-1])
+
+# Put the dates in order
+dates_ordered = sorted(dates_scrambled)
+
+# Print the first and last ordered dates
+print(dates_ordered[0])
+print(dates_ordered[-1])
---
+++ 18_Working with Dates and Times in Python/README.md
@@ -0,0 +1,9 @@
+# Intermediate Importing Data in Python
+
+[Statement Of Accomplishment]()
+
+ <p align='center'>
+  <a href="#">
+    <img src='' alt="cert-18">
+  </a>
+</p>

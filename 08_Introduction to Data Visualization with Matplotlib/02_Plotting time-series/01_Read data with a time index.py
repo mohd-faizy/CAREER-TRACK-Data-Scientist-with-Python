f0944f65e9f00905a2cb69812279ea62b95562d6
MOHD FAIZY <ifaizymohd@gmail.com>
2023-05-18 02:08:54 +0530

    update

--- 08_Introduction to Data Visualization with Matplotlib/02_Plotting time-series/01_Read data with a time index.py
+++
@@ -1,7 +0,0 @@
-'''
-
-'''# Import pandas as pd
-import pandas as pd
-
-# Read the data from file using read_csv
-climate_change = pd.read_csv('climate_change.csv', parse_dates=['date'], index_col='date')
